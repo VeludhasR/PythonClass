00987940e475192527f622cbb06da2f56728764c
justinraj <justinraj958@gmail.com>
2024-06-05 17:06:31 +0530

    day 13

---
+++ code/Day13.py
@@ -0,0 +1,22 @@
+'''
+"Sarvesh"
+"Justin"
+"Rahul"
+"Jebin"
+"Jack"
+
+this is a list of name
+
+a="Justin"
+print(a)
+for single value storage 
+'''
+
+#this is a list
+a = ["Sarvesh" , "Justin" , "Rahul" , "Jebin" , "Jack"]
+print(a)
+
+#index 
+print(a[1])
+
+
