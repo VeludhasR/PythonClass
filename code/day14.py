cd506e790f726d213b72606db7801a376e360ade
justinraj <justinraj958@gmail.com>
2024-06-08 16:04:40 +0530

    Day 14

---
+++ code/day14.py
@@ -0,0 +1,27 @@
+# append  this is a command line you can write anything 
+'''
+data = [50 , 60 ,82 , 23 , 40 , 22 , 54 ]
+
+print("Before adding data")
+print(data)
+
+#add  - 90
+
+data.append(90)
+print("After adding data")
+print(data)
+'''
+
+data = [50 , 60 ,82 , 23 , 40 , 22 , 54 ]
+
+
+print("Before removing data")
+print(data)
+
+data.remove(60)
+
+print("After removing data")
+print(data)
+
+
+
